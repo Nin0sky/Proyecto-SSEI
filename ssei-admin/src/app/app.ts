6103e5ebe78442f61af1941700a20a9d1e180c84
Nin0sky <cl.vasquezm@duocuc.cl>
2026-08-10 13:03:09 -0400

    fix: visualizacion de login en pagina inicial

--- ssei-admin/src/app/app.ts
+++ ssei-admin/src/app/app.ts
@@ -1,14 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Agregamos CommonModule para directivas condicionales
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from './core/services/auth.service'; // Importamos el servicio
 
 @Component({
   selector: 'app-root',
   imports: [
+    CommonModule, // Agregado para usar estructuras condicionales (o utilizar @if de Angular 17+)
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
@@ -22,5 +25,12 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './app.scss'
 })
 export class App {
+  // Inyectamos el servicio de Autenticación
+  protected readonly authService = inject(AuthService);
   protected readonly title = signal('SSEI Admin');
+
+  // Método de conveniencia para cerrar la sesión
+  logout(): void {
+    this.authService.logout();
+  }
 }
